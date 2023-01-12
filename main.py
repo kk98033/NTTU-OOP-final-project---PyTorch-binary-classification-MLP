0f01dd8697172ebc782299bc5770623e11f3a2b0
Chia Yu Yang <a8829037@gmail.com>
2023-01-12 15:21:57 +0800

    finish data

--- main.py
+++ main.py
@@ -19,21 +19,36 @@ from torch.nn.init import xavier_uniform_
 import time
 import copy
 
+class ThryoidCSVDataset(Dataset):
+    def __init__(self) -> None:
+        # read data
+        dataURL = 'https://raw.githubusercontent.com/StatsGary/Data/main/thyroid_raw.csv'
+        df = pd.read_csv(dataURL, header=None)   
+        print(df)
+
+        # modify data
+        self.x = df.values[:, :-1]
+        self.x = self.x.astype('float32')
+
+        self.y = df.values[:, -1]
+        self.y = numpy.array([1 if i == 'sick' else 0 for i in self.y]) # sick = 1, neg = 0
+        self.y = self.y.astype('float32')
+        self.y = self.y.reshape((len(self.y), 1))
+        print(self.x)
+        print(self.y)
+
+        print(self.splitData())
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        return [self.x[index], self.y[index]]
+
+    def splitData(self, ratio=0.2):
+        testSize = round(ratio * len(self.x))
+        trainSize = len(self.x) - testSize
+        return random_split(self, [testSize, trainSize])
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-dataURL = 'https://raw.githubusercontent.com/StatsGary/Data/main/thyroid_raw.csv'
-
-
-df = pd.read_csv(dataURL, header=None)   
-# df.head()
-# print(df)
-x = df.values[:, :-1]
-x = x.astype('float32')
-
-y = df.values[:, -1]
-y = numpy.array([1 if i == 'sick' else 0 for i in y])
-y = y.astype('float32')
-y = y.reshape((len(y), 1))
-print(x)
-print(y)
+thryoid = ThryoidCSVDataset()
