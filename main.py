606a2507b96a306713856b3b96e3b8840ce27017
Chia Yu Yang <a8829037@gmail.com>
2023-01-13 09:18:50 +0800

    add mlp class

--- main.py
+++ main.py
@@ -50,5 +50,36 @@ class ThryoidCSVDataset(Dataset):
         trainSize = len(self.x) - testSize
         return random_split(self, [testSize, trainSize])
 
+class ThyroidMLP(Module):
+    def __init__(self, nInputs) -> None:
+        ''' 
+        The rectified linear activation function or ReLU for short is a piecewise linear function that will output the input directly if it is positive, otherwise, it will output zero.
+        https://machinelearningmastery.com/rectified-linear-activation-function-for-deep-learning-neural-networks/#:~:text=The%20rectified%20linear%20activation%20function,otherwise%2C%20it%20will%20output%20zero. 
+        '''
+        super().__init__()
+        self.hidden1 = Linear(nInputs, 20) # output 20 layers
+        kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
+        self.act1 = ReLU()
+
+        self.hidden2 = Linear(20, 10) # 20 layer inputs from hidden 1
+        kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
+        self.act2 = ReLU()
+
+        self.hidden3 = Linear(10, 1) # 10 layer inputs from hidden 2
+        xavier_uniform_(self.hidden3.weight)
+        self.act3 = Sigmoid() # allows the classification to fall between 0 – 1, meaning the higher the weight, the more likely the outcome is 1 = sick, than not sick, or negative in the labels case.
+
+    def forward(self, x):
+        x = self.hidden1(x)
+        x = self.act1(x)
+
+        x = self.hidden2(x)
+        x = self.act2(x)
+
+        x = self.hidden3(x)
+        x = self.act3(x)
+
+        return x
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 thryoid = ThryoidCSVDataset()
